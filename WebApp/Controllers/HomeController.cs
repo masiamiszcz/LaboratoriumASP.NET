1e2b485f10e5d2764cf48e73399578b0dc992f83
maciej.kubacka <masiamiszcz@gmail.com>
2024-10-09 12:13:57 +0200

    Second commit  last on lab_1

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Mvc;
 using WebApp.Models;
 
@@ -25,8 +26,47 @@ public class HomeController : Controller
     {
         return View();
     }    
-    public IActionResult Calculator()
+    public IActionResult Calculator(Operator? op, decimal? x, decimal? y)
     {
+// napisz metode age, parametry : data , result wyswietla wiek, w latach miesiacach i dniach 
+
+        decimal? result = 0.0m;
+        if (x is null||y is null)
+        {
+            ViewBag.ErorMessage = "Niepoprawny format liczby x lub y";
+            return View("nullxy");
+        }
+
+        if (op is null || (op != Operator.Add && op != Operator.Div && op != Operator.Sub && op!= Operator.Mul))
+        {
+            ViewBag.ErorMessage("Nieznany operator");
+            return View("nullxy");
+
+        }
+        
+        switch (op)
+        {
+            case Operator.Add:
+                result = x + y;
+                ViewBag.Operator = "+";
+                break;
+            case Operator.Sub:
+                result = x - y;
+                ViewBag.Operator = "-";
+                break;
+            case Operator.Mul:
+                result = x * y;
+                ViewBag.Operator = "*";
+                break;
+            case Operator.Div:
+                result = x / y;
+                ViewBag.Operator = "/";
+                break;
+        }
+
+        ViewBag.Result = result;
+        ViewBag.X = x;
+        ViewBag.Y = y;
         return View();
     }
 
@@ -42,5 +82,10 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public enum Operator
+    {
+        Add, Sub, Mul, Div
     }
 }
