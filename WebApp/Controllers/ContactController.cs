3c2653414fadf38534cfc870139351ac87f71530
maciej.kubacka <masiamiszcz@gmail.com>
2024-10-23 12:14:08 +0200

    NEW COMMIT, JEST ZLE HAHHA

---
+++ WebApp/Controllers/ContactController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class ContactController : Controller
+{
+    private static Dictionary<int, ContactModel> _contacts = new()
+    {
+        {
+            1, new ContactModel()
+            {
+                Id = 1, First_name = "michal", Last_name = "krul", Email = "jestemkrulem@gmail.com", Phonenumber = "999888777", Date_of_Birth = new DateOnly(2000, 10, 30)
+            }
+        },
+        {
+        2, new ContactModel()
+        {
+            Id = 2, First_name = "michal", Last_name = "krol", Email = "niejestemkrulem@gmail.com", Phonenumber = "111222333", Date_of_Birth = new DateOnly(2002, 10, 30)
+        }}
+    };
+
+    private static int _currentId = 2;
+    // Lista kontaków
+    public IActionResult Index()
+    {
+        return View(_contacts);
+    }
+    // Formularz dodawania
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View("AddForm");
+    }
+    // odebranie danych i zapis kontaku
+    [HttpPost]
+    public IActionResult Add(ContactModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View("AddForm");
+        }
+
+        model.Id = ++_currentId;
+        _contacts.Add(model.Id, model);
+        // wyswietlanie danych
+        return View("Index", _contacts);
+    }
+
+    public IActionResult Delete(int id)
+    {
+        _contacts.Remove(id);
+        return View("Index", _contacts);
+    }
+
+    public IActionResult Details(int id)
+    {
+        return View(_contacts[id]);
+    }
+}
---
+++ WebApp/Models/ContactModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Models;
+
+public class ContactModel
+{
+    [HiddenInput]
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Musisz wpisać imię!")]
+    [MaxLength(length:20, ErrorMessage = "Max 20 znaków!")]
+    [MinLength(length:2, ErrorMessage = "Minimum 2 znaki!")]
+    public string First_name { get; set; }
+    [MaxLength(length:50,ErrorMessage = "Nazwisko")]
+    [MinLength(length:2, ErrorMessage = "Minimum 2 znaki!")]
+    public string Last_name { get; set; }
+    [EmailAddress(ErrorMessage = "Wpisz poprawny adres Email!")]
+    public string Email { get; set; }
+    [Phone(ErrorMessage = "Wprowadz poprawny numer telefonu!")]
+    [RegularExpression(pattern:"\\d{9}", ErrorMessage = "Dlugosc numeru telefon to 9 cyfr!")]
+    public string Phonenumber { get; set; }
+    [DataType(DataType.Date)]
+    public DateOnly Date_of_Birth { get; set; }
+}
