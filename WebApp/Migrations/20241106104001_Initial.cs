d36e66b68ba4b47614ea7f264e0987db7a7bafe8
maciej.kubacka <masiamiszcz@gmail.com>
2024-11-13 12:20:47 +0100

    XD

---
+++ WebApp/Migrations/20241106104001_Initial.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    First_name = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    Last_name = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    phone = table.Column<string>(type: "TEXT", nullable: false),
+                    Date_of_Birth = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    Category = table.Column<int>(type: "INTEGER", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contacts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "contacts",
+                columns: new[] { "Id", "Category", "Created", "Date_of_Birth", "Email", "First_name", "Last_name", "phone" },
+                values: new object[,]
+                {
+                    { 1, 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateOnly(2000, 10, 10), "krol@legowski.pl", "krull", "legowski", "512323123" },
+                    { 2, 0, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateOnly(2000, 10, 10), "masia@leggsdagowski.pl", "masia", "miszcz", "999666333" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contacts");
+        }
+    }
+}
